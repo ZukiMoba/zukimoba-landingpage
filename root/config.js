8cd50eeed8f40c9ca7702679c0fccd47ea0315ab
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-08-17 13:58:55 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -4,5 +4,43 @@
     "accept_close": true,
     "img_url": "/images/modal-new.jpeg",
     "link_to": "https://docs.verofarm.com/introduction/airdrops"
-  }
+  },
+  "partner": [
+    {
+      "img_url": "/images/partner/maxtech.svg",
+      "link_to": "https://maxtech.company/"
+    },
+    {
+      "img_url": "https://jlaunchpad.com/",
+      "link_to": "/images/partner/launchpad.svg"
+    },
+    {
+      "img_url": "/images/partner/horachain.svg",
+      "link_to": "https://horachain.com/"
+    },
+    {
+      "img_url": "/images/partner/horachain.svg",
+      "link_to": "https://airdropalert.com/"
+    },
+    {
+      "img_url": "/images/partner/audrog.svg",
+      "link_to": "https://airdropalert.com/"
+    },
+    {
+      "img_url": "/images/partner/dxspace.svg",
+      "link_to": "https://dxspace.org/"
+    },
+    {
+      "img_url": "/images/partner/cryptodaily.svg",
+      "link_to": "https://cryptodaily.io/"
+    },
+    {
+      "img_url": "/images/partner/ctystal.svg",
+      "link_to": "https://crystalwallet.eu/"
+    },
+    {
+      "img_url": "/images/partner/ventures.svg",
+      "link_to": "https://ssventures.capital/"
+    }
+  ]
 }
