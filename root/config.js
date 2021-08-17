e1e11861cf295f7ae1e47841d18f34ffeedbf61e
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-08-17 10:49:53 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -1,6 +1,6 @@
 {
   "banner": {
-    "is_active": false,
+    "is_active": true,
     "accept_close": true,
     "img_url": "/images/modal-new.jpeg",
     "link_to": "https://docs.verofarm.com/introduction/airdrops"
