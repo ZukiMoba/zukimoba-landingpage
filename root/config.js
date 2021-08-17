a5375410209e1aa8ee3ea6683c6869afbb852cd4
Dr Soft App <80030143+drsoftapp@users.noreply.github.com>
2021-08-17 16:26:47 +0700

    Update config.js

--- root/config.js
+++ root/config.js
@@ -58,6 +58,24 @@
       "img_url": "/images/social/any.svg",
       "link_to": "https://www.reddit.com/r/VeroFarmGame",
       "target": "_blank"
+    },
+    {
+      "label": "Discord Group",
+      "img_url": "/images/social/discord-group.svg",
+      "link_to": "https://discord.com/channels/872066519008411699/872066519008411701",
+      "target": "_blank"
+    },
+    {
+      "label": "Telegram",
+      "img_url": "/images/social/telegram.svg",
+      "link_to": "https://t.me/VeroFarm",
+      "target": "_blank"
+    },
+    {
+      "label": "Telegram Group",
+      "img_url": "/images/social/telegram-group.svg",
+      "link_to": "https://t.me/VeroFarmGroup",
+      "target": "_blank"
     }
   ]
 }
